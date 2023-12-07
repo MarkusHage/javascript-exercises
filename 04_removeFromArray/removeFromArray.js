742a77e89225bf31869f003c2fa35c3deabc20a4
MarkusHage <127237872+MarkusHage@users.noreply.github.com>
2023-12-07 20:05:09 +0100

    completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
+    array.forEach((i) => {
+        if (!args.includes(i)) {
+            newArray.push(i);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
