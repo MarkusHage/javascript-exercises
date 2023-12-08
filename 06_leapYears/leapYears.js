0b8d8dca47d0749ec71f2682b6c960e0b35566c3
MarkusHage <127237872+MarkusHage@users.noreply.github.com>
2023-12-08 16:06:19 +0100

    completed exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let check00 = String(year).slice(-2);
+    if (year % 4 == 0 && check00 != "00") {
+        return true
+    } else if (year % 400 == 0 && check00 == "00") {
+        return true
+    } else {
+        return false  
+    }
 };
 
 // Do not edit below this line
